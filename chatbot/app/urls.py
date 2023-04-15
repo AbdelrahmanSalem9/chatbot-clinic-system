4c84cfe9b6e89c1cdfda0a5448f779d1e2ed733b
AbdelrahmanSalem9 <Abdelrahmansalem16@gmail.com>
2023-04-15 16:03:33 +0200

    Add the chat page

--- chatbot/app/urls.py
+++ chatbot/app/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "app"
 urlpatterns = [
     path('',views.IndexView,name='index'),
-    path('chatbot/',views.ChatbotView,name='chatbot'),
-    # path('user/',views.UserView.as_view(),name='user')
+    path('chat/',views.ChatView,name='chat'),
+    path('chatbot/', views.chatbot, name='chatbot'),
     ]
--- chatbot/app/views.py
+++ chatbot/app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def IndexView(request):
     return render(request,template_name='app/index.html')
 
-def ChatbotView(request):
-    return render(request,template_name='app/chatbot.html')
+def ChatView(request):
+    return render(request,template_name='app/chat.html')
+
+@csrf_exempt
+def chatbot(request):
+    # Get the user's input from the AJAX request
+    user_input = request.POST.get('user_input', '')
+
+    # TODO: Implement a more advanced chatbot here
+
+    # Create a bot response
+    bot_response = 'I got your message: ' + user_input
+
+    # Send the bot response back to the JavaScript file
+    return JsonResponse({'bot_response': bot_response})
