c86a73a043972745909c4e4e5434dfb5c1600737
AbdelrahmanSalem9 <Abdelrahmansalem16@gmail.com>
2023-04-22 21:00:15 +0200

    create appointment table

--- chatbot/app/admin.py
+++ chatbot/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Doctor, Patient
+from .models import Patient, Doctor
 
 # Register your models here.
 admin.site.register(Patient)
--- chatbot/app/models.py
+++ chatbot/app/models.py
@@ -1,22 +1,35 @@
 from django.db import models
 
 # Create your models here.
-class Doctor(models.Model):
-    name = models.CharField(max_length=100)
-    specialty = models.CharField(max_length=100)
-    availability = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
-    
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=255)
+    specialty = models.CharField(max_length=100)
+    working_hours_start = models.TimeField(null=True,blank=True)
+    working_hours_end = models.TimeField(null=True,blank=True)
+    working_days = models.CharField(max_length=255,null=True)
+    about = models.TextField(blank=True)
+
+
 class Patient(models.Model):
     email = models.EmailField(primary_key=True)
     full_name = models.CharField(max_length=255)
     phone = models.CharField(max_length=20)
-    birth_date = models.DateField()
+    birth_date = models.DateField(null=True,blank=True)
     gender = models.CharField(max_length=10)
     marital_status = models.CharField(max_length=20)
     height = models.DecimalField(max_digits=5, decimal_places=2)
     weight = models.DecimalField(max_digits=5, decimal_places=2)
-    medications = models.TextField(blank=True, null=True)
-    allergies = models.TextField(blank=True, null=True)
+    medications = models.BooleanField(default=True)
+    allergies = models.BooleanField(default=True)
+
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(null=True,blank=True)
+    end_time = models.DateTimeField(null=True,blank=True)
+
+    class Meta:
+        unique_together = ('doctor', 'start_time')
