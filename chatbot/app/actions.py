c78e2fac2650a7b5b0da12c8902cc43e91bb8ffa
AbdelrahmanSalem9 <Abdelrahmansalem16@gmail.com>
2023-05-09 17:00:06 +0300

    add action file for database access

---
+++ chatbot/app/actions.py
@@ -0,0 +1,118 @@
+import json
+from django.http import JsonResponse
+from datetime import timedelta, datetime
+from .models import Appointment, Patient, Doctor, Speciality
+from datetime import datetime
+from django.utils import timezone
+
+
+class Action:
+    def __init__(self, data):
+        self.data = json.loads(data)
+
+    def parse(self):
+        pass
+
+    def execute(self):
+        pass
+
+
+class AddPatient(Action):
+    def parse(self):
+        patient_data = {
+            'full_name': self.data.get('full_name'),
+            'phone': self.data.get('contact_number'),
+            'birth_date': self.data.get('date_of_birth'),
+            'gender': self.data.get('gender'),
+            'marital_status': self.data.get('marital_status'),
+            'height': self.data.get('height'),
+            'weight': self.data.get('weight'),
+            'medications': self.data.get('medications') == 'yes',
+            'allergies': self.data.get('allergies') == 'yes',
+        }
+        return patient_data
+
+    def execute(self, email):
+        try:
+            patient_data = self.parse()
+            patient, created = Patient.objects.get_or_create(
+                email=email, defaults=patient_data)
+            if not created:
+                # Update only the fields that have changed
+                fields_to_update = {
+                    k: v for k, v in patient_data.items() if v != getattr(patient, k)}
+                if fields_to_update:
+                    Patient.objects.filter(pk=email).update(**fields_to_update)
+        except Exception as e:
+            raise Exception("Can't Save Patient Data")
+
+
+class CreateAppointment(Action):
+
+    def parse(self):
+        try:
+            return self.data['patient_email'], self.data['doctor_id'], datetime.strptime(self.data['time_slot'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        except:
+            raise Exception("Can't Parse Data")
+
+    def execute(self):
+        patient_email, doctor_id, time_slot = self.parse()
+        print(time_slot)  # 2023-05-03 09:00:00
+        try:
+            doctor = Doctor.objects.get(pk=doctor_id)
+            appointment = Appointment(
+                doctor=doctor,
+                patient=Patient.objects.get(pk=patient_email),
+                start_time=time_slot + timedelta(minutes=180),
+                end_time=time_slot + timedelta(minutes=210),
+            )
+            appointment.save()
+            return JsonResponse({'message': 'Appointment booked successfully', 'appointment_id': appointment.pk})
+
+        except Exception as e:
+            print(e)
+            raise Exception("Can't Create Appointment")
+
+
+class ModifyAppointment(Action):
+
+    def parse(self):
+        try:
+            return self.data['date'], datetime.strptime(self.data['time_slot'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        except:
+            raise Exception("Can't Parse Data")
+
+    def execute(self, id):
+        try:
+            selected_date, time_slot = self.parse()
+            print(time_slot, type(time_slot))
+            appointment = Appointment.objects.get(pk=id)
+            appointment.start_time = time_slot + timedelta(minutes=180)
+            appointment.end_time = time_slot + timedelta(minutes=210)
+            appointment.save()
+            return JsonResponse({'message': 'Appointment modified successfully', 'appointment_id': appointment.pk})
+        except Exception as e:
+            print(e)
+            raise Exception("Can't Modify Appointment")
+
+
+class DeleteAppointment(Action):
+    def parse(self):
+        try:
+            return self.data['appointment_id'], self.data['doctor_id'], self.data['patient_email']
+        except:
+            raise Exception("Can't Parse Data")
+
+    def execute(self):
+        appointment_id, doctor_id, patient_email = self.parse()
+        try:
+            appointment = Appointment.objects.select_related('doctor', 'patient').get(
+                pk=appointment_id,
+                doctor__pk=doctor_id,
+                patient__email=patient_email
+            )
+            appointment.delete()
+            return JsonResponse({'message': 'Appointment deleted successfully', 'deleted': True})
+        except Exception as e:
+            print(e)
+            raise Exception("Can't Delete Appointment")
--- chatbot/app/views.py
+++ chatbot/app/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, JsonResponse
-from .models import Patient, Doctor, Appointment, Speciality
-from django.utils import timezone
-from datetime import timedelta, datetime
-import json
-from .utils import get_available_slots
-
-
-# TODO: REPLACE JQUERY WITH VANILLA JS
-
+from django.http import JsonResponse
+from .models import Doctor, Appointment, Speciality
+from .utils import get_available_slots, create_appointment, modify_appointment, delete_appointment
+from .actions import CreateAppointment, ModifyAppointment, DeleteAppointment, AddPatient
 
 from .bot import Bot
 
@@ -21,6 +15,7 @@ def index_view(request):
 
 def chat_view(request):
     return render(request, template_name='app/chat.html')
+
 
 def chatbot(request):
     # Get the user's input from the AJAX request
@@ -36,31 +31,10 @@ def chatbot(request):
     return JsonResponse({'bot_response': bot_response, 'link': None})
 
 
-def appointment(request):
+def patient_registeration(request):
     if request.method == 'POST':
-        post_data = request.POST
-        email = post_data.get('email')
-        patient_data = {
-            'full_name': post_data.get('full_name'),
-            'phone': post_data.get('contact_number'),
-            'birth_date': post_data.get('date_of_birth'),
-            'gender': post_data.get('gender'),
-            'marital_status': post_data.get('marital_status'),
-            'height': post_data.get('height'),
-            'weight': post_data.get('weight'),
-            'medications': post_data.get('medications') == 'yes',
-            'allergies': post_data.get('allergies') == 'yes',
-        }
-
-        patient, created = Patient.objects.get_or_create(
-            email=email, defaults=patient_data)
-        if not created:
-            # Update only the fields that have changed
-            fields_to_update = {
-                k: v for k, v in patient_data.items() if v != getattr(patient, k)}
-            if fields_to_update:
-                Patient.objects.filter(pk=email).update(**fields_to_update)
-
+        email = request.POST.get(email)
+        AddPatient(request.POST).execute(email)
         return redirect(f'/book_appointment/?email={email}')
 
     return render(request, template_name='app/appointment.html')
@@ -89,12 +63,8 @@ def get_doctors(request):
 
 def book_appointment(request):
     if request.method == 'POST':
-        id = new_appointment(request.body)
-        if id:
-            return JsonResponse({'message': 'Appointment booked successfully', 'appointment_id': id})
-        return JsonResponse({'error': 'Appointment NOT booked'})
+        return CreateAppointment(request.body).execute()
     else:
-        # Display the form to book an appointment
         doctors = Doctor.objects.all()
         specialties = Speciality.objects.all()
         context = {
@@ -105,63 +75,19 @@ def book_appointment(request):
         return render(request, 'app/book_appointment.html', context)
 
 
-def delete(request):
+def cancel(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        try:
-            appointment = Appointment.objects.select_related('doctor', 'patient').get(
-                pk=data['appointment_id'],
-                doctor__pk=data['doctor_id'],
-                patient__email=data['patient_email']
-            )
-            appointment.delete()
-            return JsonResponse({'message': 'Appointment deleted successfully', 'deleted': 'True'})
-        except Appointment.DoesNotExist:
-            pass
-        return JsonResponse({'error': 'Appointment not found'})
-
-    doctors = Doctor.objects.all()
-    context = {'doctors': doctors}
-    return render(request, template_name='app/delete.html', context=context)
+        return DeleteAppointment(request.body).execute()
+    else:
+        doctors = Doctor.objects.all()
+        context = {'doctors': doctors}
+        return render(request, template_name='app/delete.html', context=context)
 
 
-def new_appointment(data):
-    try:
-        data = json.loads(data)
-        doctor_id = data['doctor_id']
-        selected_date = data['date']
-        time_slot = datetime.strptime(
-            data['time_slot'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        doctor = Doctor.objects.get(pk=doctor_id)
-        appointment = Appointment(
-            doctor=doctor,
-            patient=Patient.objects.get(pk=data['patient_email']),
-            start_time=time_slot + timedelta(minutes=180),
-            end_time=time_slot + timedelta(minutes=210),
-        )
-        appointment.save()
-        return appointment.pk
-    except Exception as e:
-        print("ERROR is happened when saving new appointment")
-        print(e)
-        return False
-
-
-def modify_appointment(request):
+def change(request):
     id = request.GET.get('id')
     if request.method == 'POST':
-        try:
-            appointment = Appointment.objects.get(pk=id)
-            data = json.loads(request.body)
-            appointment.selected_date = data['date']
-            appointment.time_slot = datetime.strptime(
-                data['time_slot'], '%Y-%m-%dT%H:%M:%S.%fZ')
-            appointment.save()
-            return JsonResponse({'message': 'Appointment modified successfully'})
-        except Appointment.DoesNotExist:
-            return JsonResponse({'message': 'Appointment does not exist'}, status=404)
-        except:
-            return JsonResponse({'message': 'Failed to modify appointment'}, status=500)
+        return ModifyAppointment(request.body).execute(id)
     else:
         appointment = Appointment.objects.get(pk=id)
         context = {
