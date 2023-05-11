5f960c2159040ec40d0e72deb9aa51db972294b0
AbdelrahmanSalem9 <Abdelrahmansalem16@gmail.com>
2023-05-11 04:28:02 +0300

    fix query prioirty sequence

--- chatbot/app/bot.py
+++ chatbot/app/bot.py
@@ -1,6 +1,8 @@
 from .query_handler import QueryHandler
 from nltk.chat.util import Chat, reflections
 
+# TODO: Prioirty of words in the user input
+
 
 class Bot:
     def __init__(self):
@@ -11,8 +13,8 @@ class Bot:
             (r'how are you?', [
              'I am doing well, thank you!', 'I am fine, thank you!']),
             (r'bye|goodbye|see you', ['Goodbye!', 'See you later.']),
-            (r'modify|change|adjust|reform', [
-             "Sure, I will help you to modify an appointment now, provide me with the appointment ID."]),
+            # (r'modify|change|adjust|reform', [
+            #  "Sure, I will help you to modify an appointment now, provide me with the appointment ID."]),
             (r'(.*)', ['I am sorry, I did not understand.']),
 
         ]
@@ -21,20 +23,21 @@ class Bot:
         self.appointment_keywords = (
             'appointment', 'designation', 'scheduling', 'schedule')
         self.doctor_keywords = ('doctor', 'doc', 'physician', 'Dr.', 'ph')
+        self.modify_keywords = ('modify', 'change', 'adjust', 'reform')
         self.delete_keywords = ('delete', 'remove', 'cancel')
         self.specialities_keywords = (
-            "specialities", "speciality", "specialty", "doctors")
+            "specialities", "speciality", "specialty")
 
     def get_response(self, user_input):
-        if any(s in user_input for s in self.doctor_keywords):
-            return self.handler.doctor_query(user_input)
+        if any(s in user_input for s in self.delete_keywords):
+            return self.handler.delete_appointment_query(user_input)
+        elif any(s in user_input for s in self.modify_keywords):
+            return self.handler.modify_appointment_query(user_input)
         elif any(s in user_input for s in self.appointment_keywords):
             return self.handler.new_appointment_query()
-        elif any(s in user_input for s in self.delete_keywords):
-            return self.handler.delete_appointment_query(user_input)
-        elif '#' in user_input:
-            return self.handler.modify_appointment_query(user_input)
         elif any(s in user_input for s in self.specialities_keywords):
             return self.handler.speciality_query()
+        if any(s in user_input for s in self.doctor_keywords):
+            return self.handler.doctor_query(user_input)
         else:
             return self.chat.respond(user_input)
--- chatbot/app/query_handler.py
+++ chatbot/app/query_handler.py
@@ -1,4 +1,5 @@
 from .models import Doctor, WorkingDay, Appointment
+import re
 
 
 class QueryHandler:
@@ -7,17 +8,22 @@ class QueryHandler:
         for doctor in doctors:
             if doctor.name.lower() in user_input.lower():
                 return f"{doctor.get_info()} Working days: {self._get_working_days(doctor)}"
+        else:
+            return self.speciality_query()
 
     def _get_working_days(self, doctor):
         return " and ".join([str(working_day.get_info()) for working_day in WorkingDay.objects.filter(doctor=doctor)])
 
     def modify_appointment_query(self, user_input):
-        id = user_input[1:]
-        try:
-            appointment = Appointment.objects.get(pk=id)
-            return f"I will help you modfiy you appointment now...", f"/change/?id={id}"
-        except:
-            return f"Sorry, I could not find the appointment with id {user_input}. Please try again."
+        id = re.search(r'#(\d+)', user_input).group(1)
+        if id:
+            try:
+                appointment = Appointment.objects.get(pk=id)
+                return f"I will help you modfiy you appointment now...", f"/change/?id={id}"
+            except:
+                return f"Sorry, I could not find the appointment with id: {id}. Please try again."
+        else:
+            return f"Please provide me with the appointment ID."
 
     def new_appointment_query(self):
         return f"Sure, I will take you to the appointment page now...", "/patient_registeration"
